ff5eb8ce0a72b3faad2dcb25ffa0005c4134248c
Daniel Cu Sanchez <danielcu@alternet.com.mx>
2021-09-04 18:33:13 -0500

    vercel json

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.json());
